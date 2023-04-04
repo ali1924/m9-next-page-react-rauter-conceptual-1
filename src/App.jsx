b6c4136410a7a573d6c9b5742b3b2ced580d7171
ali1924 <mohammadali229987@gmail.com>
2023-04-04 21:56:08 +0600

    about https://github.com/shakilahmedatik/nextPage-complete/blob/main/src/components/About.jsx  done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 const App = () =>{
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      {/* Header */}
+      <Header/>
       <Outlet></Outlet>
+
+      {/* footer */}
     </div>
   );
 };
